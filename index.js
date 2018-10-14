533e0e33f081cfe6cb468ffc9f025c7eda561a22
archana <archana.nayak@gmail.com>
2018-10-14 15:24:28 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({'hi': 'there'});
+});
+
+//process.env.PORT for production where HEROKU will
+//set the underlying environment variable.
+// For development, we define the port at which our
+//server will run on.
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, () => {
+  console.log('listening on port 5000');
+});
