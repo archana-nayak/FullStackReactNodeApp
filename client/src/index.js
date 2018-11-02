e5eab3c3f4d2480f1ad4b08acefb957103c5a5b0
archana <archana.nayak@gmail.com>
2018-11-01 21:36:11 -0700

    save all changes to client files

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,5 @@
+import materializeCss from 'materialize-css/dist/css/materialize.min.css';
+
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
@@ -5,12 +7,13 @@ import { createStore, applyMiddleware} from 'redux';
 
 import App from './components/App';
 import reducer from './reducers';
+import reduxThunk from 'redux-thunk';
 
 //dummy reducer that returns an array ;() => []
 //initial state of the store 
 //invoke the applyMiddleware, right now there is no 
 // middleware but will be replaced by redux thunk
-const store = createStore(reducer, {}, applyMiddleware());
+const store = createStore(reducer, {}, applyMiddleware(reduxThunk));
 
 
 ReactDom.render(
